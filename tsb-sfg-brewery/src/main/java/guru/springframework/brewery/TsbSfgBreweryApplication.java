4124cba31ce8db576ed1148a2b05fad303533426
kenny_alvarado <akennsteven@gmail.com>
2023-01-28 14:32:56 +0100

    adding circle ci

---
+++ tsb-sfg-brewery/src/main/java/guru/springframework/brewery/TsbSfgBreweryApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.brewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsbSfgBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsbSfgBreweryApplication.class, args);
+	}
+
+}
